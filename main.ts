c567958d99085542966dd4c3e2c3abc346e3c9b5
Ricardo Robles <rroblf01@estudiantes.unileon.es>
2023-01-12 20:34:22 +0100

    example

--- main.ts
+++ main.ts
@@ -7,14 +7,11 @@ import { typeDefs } from "./typedfs.ts";
 
 const schema = makeExecutableSchema({ resolvers, typeDefs });
 
-const server = new Server(async (req) => {
+Server(async (req) => {
       const { pathname } = new URL(req.url);
   
       return await GraphQLHTTP<Request>({
           schema,
           graphiql: true,
         })(req)
-    }
-  );
-  
-  server.listenAndServe();
+    });
