dd717413cb2161937d9b05a70ab72faa5c2e4140
Ricardo Robles <rroblf01@estudiantes.unileon.es>
2023-01-13 10:42:16 +0100

    Add deno env

--- db/ddbb.ts
+++ db/ddbb.ts
@@ -1,33 +1,11 @@
 import { MongoClient } from "../deps.ts";
-import { config } from "../config/config.ts";
 import { Dinosaur } from "../models/dinosaur.ts";
 
 const client = new MongoClient();
-const uri =
-  `mongodb+srv://${config.MONGO_USER}:${config.MONGO_PASSWORD}@cluster0.u8kicjc.mongodb.net/?authMechanism=SCRAM-SHA-1`;
+const uri = `mongodb+srv://${Deno.env.get("MONGO_USER")}:${
+  Deno.env.get("MONGO_PASSWORD")
+}@cluster0.u8kicjc.mongodb.net/?authMechanism=SCRAM-SHA-1`;
 await client.connect(uri);
 
 const db = client.database("deno-graphql");
 export const dinosaurs = db.collection<Dinosaur>("dinosaurs");
-// export const dinosaurs = [
-//   {
-//     id: 0,
-//     name: "Tyrannosaurus",
-//     description: "A large carnivorous dinosaur",
-//   },
-//   {
-//     id: 1,
-//     name: "Stegosaurus",
-//     description: "A large herbivorous dinosaur",
-//   },
-//   {
-//     id: 2,
-//     name: "Velociraptor",
-//     description: "A most feared dinosaur",
-//   },
-//   {
-//     id: 3,
-//     name: "Triceratops",
-//     description: "A large herbivorous dinosaur",
-//   },
-// ];
--- deps.ts
+++ deps.ts
@@ -7,4 +7,4 @@ export { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 export { GraphQLHTTP } from "https://deno.land/x/gql@1.1.2/mod.ts";
 export { makeExecutableSchema } from "https://deno.land/x/graphql_tools@0.0.2/mod.ts";
 export { gql } from "https://deno.land/x/graphql_tag@0.0.1/mod.ts";
-import * as gql_render from "https://deno.land/x/gql@1.1.2/graphiql/render.ts";
+export * as gql_render from "https://deno.land/x/gql@1.1.2/graphiql/render.ts";
