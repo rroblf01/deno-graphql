f435bc5bc59f9af2bcecce9602efe98f69c15fa4
Ricardo Robles <rroblf01@estudiantes.unileon.es>
2023-01-13 10:35:06 +0100

    import gql render

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
-import { serve } from "./deps.ts"
-import { GraphQLHTTP } from "./deps.ts"
-import { makeExecutableSchema } from "./deps.ts"
+import { serve } from "./deps.ts";
+import { GraphQLHTTP } from "./deps.ts";
+import { makeExecutableSchema } from "./deps.ts";
 import { resolvers } from "./graphql/resolvers.ts";
 import { typeDefs } from "./graphql/typedfs.ts";
-
+import { gql_render } from "./deps.ts";
 const schema = makeExecutableSchema({ resolvers, typeDefs });
 
 async function handler(request: Request) {
